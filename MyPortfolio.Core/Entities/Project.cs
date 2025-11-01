079879f1f05768036f5e66034a165fb8a52d7c6f
Javeedsly <cavidsly@gmail.com>
2025-11-02 02:08:35 +0400

    Add project files.

---
+++ MyPortfolio.Core/Entities/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Core.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string ProjectUrl { get; set; }
+        public string SourceCodeUrl { get; set; }
+        public List<string> Technologies { get; set; } = new List<string>();
+    }
+}
