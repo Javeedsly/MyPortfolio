ab45b6f4cb01f99fcfdc4caef3b46fd2622ad1b1
Javeedsly <cavidsly@gmail.com>
2025-11-02 12:12:48 +0400

    recover bugs(Business layer for automapper)

--- MyPortfolio.Core/DTOs/ProjectDtos.cs
+++ MyPortfolio.Core/DTOs/ProjectDtos.cs
@@ -27,7 +27,7 @@ namespace MyPortfolio.Core.DTOs
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string ProjectUrl { get; set; }
         public IFormFile? ImageFile { get; set; }
         public string? SourceCodeUrl { get; set; }
         public List<string> Technologies { get; set; } = new();
